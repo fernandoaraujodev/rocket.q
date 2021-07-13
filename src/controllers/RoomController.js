1d2d39986644f243236759ba488d04b3daa7316b
Fernando Araujo <fernandovcaraujo@gmail.com>
2021-07-13 11:04:05 -0300

    Visual para sala sem perguntas implementado

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -44,9 +44,17 @@ module.exports = {
         const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
         const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
         
+        let isNoQuestions
+        if(questions.length == 0){
+            if(questionsRead.length == 0){
+                isNoQuestions = true
+            }
+        }
+
         res.render('room', {roomId: roomId, 
                             questions: questions,
-                            questionsRead: questionsRead})
+                            questionsRead: questionsRead,
+                            isNoQuestions: isNoQuestions})
     },
 
     enter(req, res){
